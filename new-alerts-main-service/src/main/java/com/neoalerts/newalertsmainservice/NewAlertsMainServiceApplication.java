b267f36ab34dbec035e30b33b87fe868831beaeb
ronsane <ronsanity.gaming@gmail.com>
2023-11-30 19:49:27 +0530

    Added main service and user service

---
+++ new-alerts-main-service/src/main/java/com/neoalerts/newalertsmainservice/NewAlertsMainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.neoalerts.newalertsmainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAlertsMainServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewAlertsMainServiceApplication.class, args);
+    }
+
+}
---
+++ new-alerts-main-service/src/test/java/com/neoalerts/newalertsmainservice/NewAlertsMainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoalerts.newalertsmainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAlertsMainServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
